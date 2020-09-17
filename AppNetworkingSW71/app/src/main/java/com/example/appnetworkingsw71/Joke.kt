530239f6109564114f11be084769770e05ffadab
davquev <ingdavquev@gmail.com>
2020-09-17 14:54:21 -0500

    Ejemplo de NetWorking con Retrofit

---
+++ AppNetworkingSW71/app/src/main/java/com/example/appnetworkingsw71/Joke.kt
@@ -0,0 +1,8 @@
+package com.example.appnetworkingsw71
+
+import com.google.gson.annotations.SerializedName
+
+class Joke (
+    @SerializedName("joke")
+    var joke: String
+)
---
+++ AppNetworkingSW71/app/src/main/java/com/example/appnetworkingsw71/JokeActivity.kt
@@ -0,0 +1,45 @@
+package com.example.appnetworkingsw71
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_joke.*
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
+
+class JokeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_joke)
+
+        btJoke.setOnClickListener {
+            loadJoke()
+        }
+    }
+
+    private fun loadJoke() {
+        //tvJoke.text ="Chiste encontrado!!!"
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://geek-jokes.sameerkumar.website/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val jokeService : JokeService
+        jokeService = retrofit.create(JokeService::class.java)
+
+        val request = jokeService.getJoke("json")
+
+        request.enqueue(object : Callback<Joke>{
+            override fun onFailure(call: Call<Joke>, t: Throwable) {
+                Log.d("jokeActivity", t.toString())
+            }
+
+            override fun onResponse(call: Call<Joke>, response: Response<Joke>) {
+                if (response.isSuccessful){
+                    tvJoke.text = response.body()!!.joke
+                }
+            }
+
+        })
+    }
+}
